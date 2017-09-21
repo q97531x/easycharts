3f7b88beb0bd67712422ed0a9c65f87976bc46ff
64531 <bo.wei@healthbok.com>
2017-09-21 15:32:15 +0800

    init

--- easycharts-library/src/main/java/com/weeboos/easycharts_library/LineChartView.java
+++ easycharts-library/src/main/java/com/weeboos/easycharts_library/LineChartView.java
@@ -1,6 +1,7 @@
 package com.weeboos.easycharts_library;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
@@ -20,5 +21,10 @@ public class LineChartView extends AbstractChartView {
 
     public LineChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
--- easycharts-library/src/main/java/com/weeboos/easycharts_library/bean/PointValue.java
+++ easycharts-library/src/main/java/com/weeboos/easycharts_library/bean/PointValue.java
@@ -5,11 +5,44 @@ package com.weeboos.easycharts_library.bean;
  */
 
 public class PointValue {
+    private float x;
+    private float y;
+    private String label;       //说明文字
 
     public PointValue() {
+        setValue(0,0);
+    }
+
+    public PointValue(float x,float y){
+        setValue(x,y);
     }
 
     public void setValue(float x,float y){
+        setX(x);
+        setY(y);
+    }
 
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
     }
 }
