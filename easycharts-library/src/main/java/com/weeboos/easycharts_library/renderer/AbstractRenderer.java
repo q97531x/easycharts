baa15f849a429a06612af68040c1150961784595
64531 <bo.wei@healthbok.com>
2017-09-30 18:17:49 +0800

    增加渲染器

---
+++ easycharts-library/src/main/java/com/weeboos/easycharts_library/renderer/AbstractRenderer.java
@@ -0,0 +1,27 @@
+package com.weeboos.easycharts_library.renderer;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by bo.wei on 2017/9/21.
+ */
+
+public class AbstractRenderer {
+
+    private String label;
+
+    private Paint labelPaint;
+
+    public AbstractRenderer() {
+        labelPaint = new Paint();
+    }
+
+    public void setLabelPaint(Paint paint){
+        labelPaint = paint;
+    }
+
+    public void drawLabel(Canvas canvas,float x,float y){
+        canvas.drawText(label,x,y,labelPaint);
+    }
+}
---
+++ easycharts-library/src/main/java/com/weeboos/easycharts_library/renderer/AxisRenderer.java
@@ -0,0 +1,16 @@
+package com.weeboos.easycharts_library.renderer;
+
+import android.graphics.Paint;
+
+/**
+ * Created by bo.wei on 2017/9/22.
+ * 轴渲染器
+ */
+
+public class AxisRenderer extends AbstractRenderer{
+    private Paint axisPaint;
+    public AxisRenderer() {
+    }
+
+
+}
